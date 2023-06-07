e14d1d7414d519de02f5c5ffb58e9d70abe8e954
shaquille2300 <90805826+shaquille2300@users.noreply.github.com>
2023-06-06 20:38:09 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript dfhdhf</h1>`;
+appDiv.innerHTML = `<h1>TypeScript code goes here...</h1>`;
